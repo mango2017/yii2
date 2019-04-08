c22c562adc8d4dd78e75ad46a88da17f89bd564a
mango <1210056515@qq.com>
2019-04-08 10:21:02 +0800

    学习yii2

--- backend/config/main.php
+++ backend/config/main.php
@@ -12,6 +12,7 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
 //    'modules' => [],
+    'language'=>'zh-CN',//英文的home变成中文的首页
     'modules' => [
         'admin' => [
             'class' => 'app\modules\admin\AdminModule',
--- backend/views/post/view.php
+++ backend/views/post/view.php
@@ -7,7 +7,7 @@ use yii\widgets\DetailView;
 /* @var $model common\models\Post */
 
 $this->title = $model->title;
-$this->params['breadcrumbs'][] = ['label' => 'Posts', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => '文章管理', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
@@ -16,16 +16,16 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
-        <?= Html::a('Delete', ['delete', 'id' => $model->id], [
+        <?= Html::a('修改', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('删除', ['delete', 'id' => $model->id], [
             'class' => 'btn btn-danger',
             'data' => [
-                'confirm' => 'Are you sure you want to delete this item?',
+                'confirm' => '您确定要删除这篇文章吗?',
                 'method' => 'post',
             ],
         ]) ?>
     </p>
-
+    <!--显示一条数据的-->
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
@@ -33,11 +33,19 @@ $this->params['breadcrumbs'][] = $this->title;
             'title',
             'content:ntext',
             'tags:ntext',
-            'status',
-            'create_time:datetime',
-            'update_time:datetime',
-            'author_id',
+//            'status',
+            ['label'=>'状态',
+             'value'=>$model->status0->name,
+            ],
+          //  'create_time:datetime',
+            ['attribute'=>'create_time', 'value'=>date('Y-m-d H:i:s',$model->create_time)],
+          // 'update_time:datetime',
+            ['attribute'=>'update_time', 'value'=>date('Y-m-d H:i:s',$model->update_time)],
+//            'author_id',
+            ['attribute'=>'author_id', 'label'=>'作者ID', 'value'=>$model->author->nickname,],
         ],
+        'template' => '<tr><th style="width: 120px;">{label}</th><td>{value}</td></tr>',  //设置表格
+        'options'=>['class'=>'table table-striped table-bordered detail-view'],  //设置table
     ]) ?>
 
 </div>
